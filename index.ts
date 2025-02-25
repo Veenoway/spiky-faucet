138dfc4ba911575e866b1091c77ea25a6880f4de
Veenoway <77930262+Veenoway@users.noreply.github.com>
2025-02-25 13:08:30 +0100

    feature: new channel

--- index.ts
+++ index.ts
@@ -1,9 +1,4 @@
-import {
-  Client,
-  GatewayIntentBits,
-  Message,
-  PermissionsBitField,
-} from "discord.js";
+import { Client, GatewayIntentBits, Message } from "discord.js";
 import dotenv from "dotenv";
 import { ethers } from "ethers";
 dotenv.config({ path: ".env.local" });
@@ -16,6 +11,7 @@ const client = new Client({
   ],
 });
 
+// Variables d'environnement
 const DISCORD_TOKEN: string | undefined = process.env.DISCORD_TOKEN;
 const PROVIDER_URL: string | undefined = process.env.PROVIDER_URL;
 const PRIVATE_KEY: string | undefined = process.env.PRIVATE_KEY;
@@ -34,6 +30,19 @@ const provider = new ethers.JsonRpcProvider(PROVIDER_URL);
 const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
 const faucetContract = new ethers.Contract(CONTRACT_ADDRESS, ABI, wallet);
 
+const claimCooldown: Map<string, number> = new Map();
+
+const allowedChannelIds: string[] = ["1343914711460347945"];
+
+const allowedGiveMonRoleIds: string[] = [
+  "1202897827232219176",
+  "1260311106992476331",
+  "1218983061014839346",
+];
+
+let totalSent: bigint = ethers.parseEther("0");
+const MAX_SENT: bigint = ethers.parseEther("1");
+
 const isValidAddress = (address: string): boolean => {
   try {
     ethers.getAddress(address);
@@ -45,6 +54,7 @@ const isValidAddress = (address: string): boolean => {
 
 client.on("messageCreate", async (message: Message) => {
   if (message.author.bot) return;
+  if (!allowedChannelIds.includes(message.channel.id)) return;
 
   const args = message.content.split(" ");
   const command = args[0];
@@ -59,9 +69,36 @@ client.on("messageCreate", async (message: Message) => {
       await message.react("❌");
       return;
     }
+
+    const userId = message.author.id;
+    const now = Date.now();
+    const cooldownDuration = 24 * 60 * 60 * 1000;
+
+    if (claimCooldown.has(userId)) {
+      const lastClaim = claimCooldown.get(userId)!;
+      if (now < lastClaim + cooldownDuration) {
+        await message.reply("Faucet can be claimed once every 24h.");
+        return;
+      }
+    }
+
+    const faucetAmount: bigint = ethers.parseEther("0.2");
+    if (totalSent + faucetAmount > MAX_SENT) {
+      await message.reply(
+        "Faucet reached his daily limit. No more tokens can be sent."
+      );
+      return;
+    }
+
     try {
-      const tx = await faucetContract.faucet(userEthAddress);
+      const nonce = await provider.getTransactionCount(
+        wallet.address,
+        "pending"
+      );
+      const tx = await faucetContract.faucet(userEthAddress, { nonce });
       await tx.wait();
+      claimCooldown.set(userId, now);
+      totalSent += faucetAmount;
       await message.react("✅");
     } catch (error: any) {
       console.error("Error while calling faucet:", error);
@@ -70,8 +107,11 @@ client.on("messageCreate", async (message: Message) => {
   }
 
   if (command === "!give-mon") {
+    await message.member?.fetch();
     if (
-      !message.member?.permissions.has(PermissionsBitField.Flags.Administrator)
+      !message.member?.roles.cache.some((role) =>
+        allowedGiveMonRoleIds.includes(role.id)
+      )
     ) {
       await message.react("❌");
       return;
@@ -86,19 +126,53 @@ client.on("messageCreate", async (message: Message) => {
       await message.react("❌");
       return;
     }
-    let amount;
+    let amount: bigint;
     try {
       amount = ethers.parseUnits(amountInput, 18);
     } catch (error: any) {
       await message.react("❌");
       return;
     }
+    if (totalSent + amount > MAX_SENT) {
+      await message.reply(
+        "Faucet reached his daily limit. No more tokens can be sent."
+      );
+      return;
+    }
     try {
-      const tx = await faucetContract.giveMon(targetAddress, amount);
+      const nonce = await provider.getTransactionCount(
+        wallet.address,
+        "pending"
+      );
+      const tx = await faucetContract.giveMon(targetAddress, amount, { nonce });
       await tx.wait();
+      totalSent += amount;
       await message.react("✅");
     } catch (error: any) {
       console.error("Error while calling giveMon:", error);
+      await message.react("❌");
+    }
+  }
+
+  if (command === "!daily") {
+    const remaining: bigint = MAX_SENT - totalSent;
+    if (remaining <= ethers.parseEther("0")) {
+      await message.reply("Faucet reached his daily limit.");
+    } else {
+      const formattedRemaining = ethers.formatEther(remaining);
+      await message.reply(
+        `Remaining tokens for today: ${formattedRemaining} MON`
+      );
+    }
+  }
+
+  if (command === "!balance") {
+    try {
+      const balance = await provider.getBalance(CONTRACT_ADDRESS);
+      const formattedBalance = ethers.formatEther(balance);
+      await message.reply(`Contract balance: ${formattedBalance} MON`);
+    } catch (error: any) {
+      console.error("Error while retrieving contract balance:", error);
       await message.react("❌");
     }
   }
